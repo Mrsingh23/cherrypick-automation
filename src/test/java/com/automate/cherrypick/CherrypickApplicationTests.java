d71cd4577bceb986c72f86b92948b12955a81c94
sawa <sawa.malii@gmail.com>
2024-05-02 23:28:51 -0500

    first commit

---
+++ src/test/java/com/automate/cherrypick/CherrypickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automate.cherrypick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherrypickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
